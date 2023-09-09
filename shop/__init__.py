8f5aacdf048f26dc272df21ff0aecdadd82a4850
Sequence-94 <53806574+Sequence-94@users.noreply.github.com>
2023-09-09 19:18:29 +0200

    Added stripe API ,to be able to take real payment from users

--- shop/__init__.py
+++ shop/__init__.py
@@ -3,12 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_bootstrap import Bootstrap5
 from flask_login import LoginManager
-
+import os
 
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shop.db"
 app.config['SECRET_KEY'] = 'd4d2ac62ad021343b05f57dd'
+app.config['STRIPE_PUBLIC_KEY'] = "pk_test_51NoAqSGloACtem36CS01mEX1b3odSPEKjX05Zw0ab7daOWkIfBu98O27YCvuPcGYmwRIuIqtxTrTizw2pW9gQ4Fa00uwy0stSF"
+app.config['STRIPE_SECRET_KEY'] = "sk_test_51NoAqSGloACtem36BSZdAYPyEwfkeh6CwN1DSRQddp1RGQTZ8IbU5NM2lY3nj1oXnti21jeVfndBY0QXg8cVl2DI00Du37g3R9"
 # create the extension
 db = SQLAlchemy(app)
 Bootstrap5(app)
--- shop/routes.py
+++ shop/routes.py
@@ -1,15 +1,22 @@
 from shop import app
-from flask import render_template, redirect, url_for, flash,request
-from shop.models import Item, User,Cart
-from shop.forms import RegisterForm,LoginForm,PurchaseForm, CartForm, RemoveCartForm
+from flask import render_template, redirect, url_for, flash, request
+from shop.models import Item, User, Cart
+from shop.forms import RegisterForm, LoginForm, PurchaseForm, CartForm, RemoveCartForm
 from shop import db
-from flask_login import login_user,logout_user,login_required,current_user
+from flask_login import login_user, logout_user, login_required, current_user
+import stripe
+import os
+import flask
+
+YOUR_DOMAIN = 'http://127.0.0.1:5000'
+stripe.api_key = "sk_test_51NoAqSGloACtem36BSZdAYPyEwfkeh6CwN1DSRQddp1RGQTZ8IbU5NM2lY3nj1oXnti21jeVfndBY0QXg8cVl2DI00Du37g3R9"
 
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
+
 
 @app.route("/about")
 def about():
@@ -25,8 +32,8 @@ def shop_page():
         c_item_object = Item.query.filter_by(name=item_to_cart).first()
         if c_item_object:
             new_cart = Cart(
-                customer_id = current_user.id,
-                item_id = c_item_object.id
+                customer_id=current_user.id,
+                item_id=c_item_object.id
             )
             db.session.add(new_cart)
             db.session.commit()
@@ -50,7 +57,8 @@ def cart_page():
     for item in current_user_cart:
         total += item.price
 
-    return render_template('cart.html', current_user_cart=current_user_cart,total=total ,remove_cart_form =remove_cart_form )
+    return render_template('cart.html', current_user_cart=current_user_cart, total=total,
+                           remove_cart_form=remove_cart_form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -60,7 +68,7 @@ def login():
         attempted_user = User.query.filter_by(username=form.username.data).first()
         if attempted_user and attempted_user.check_password_correct(attempted_password=form.password.data):
             login_user(attempted_user)
-            flash(message=f'Success! You are logged in as: { attempted_user.username }', category='scuccess')
+            flash(message=f'Success! You are logged in as: {attempted_user.username}', category='scuccess')
             return redirect(url_for('shop_page'))
         else:
             flash(message='Username and password do not match! Please try agan', category='danger')
@@ -99,3 +107,62 @@ def register():
         for err_msg in form.errors.values():
             flash(f'There was an error creating the user: {err_msg}', category='danger')
     return render_template("register.html", form=form)
+
+
+@app.route("/create-checkout-session", methods=['GET', 'POST'])
+def create_checkout_session():
+    customer_items = []
+    input_string = request.form.get('products')
+    # Split the string into individual items and remove "<Item " and ">"
+    items = input_string.split(', ')
+    customer_products = [int(item.replace('<Item ', '').replace('>', '').replace('[', '').replace(']', '')) for item in
+                         items]
+
+    for product_id in customer_products:
+        print(f"product id:{product_id}")
+        print(f"name of product:{Item.query.filter_by(id=product_id).first().name}")
+        stripe_product_id = stripe.Product.create(
+            name=Item.query.filter_by(id=product_id).first().name,
+            # description=Item.query.filter_by(id=customer_products[product_id]).first().description,
+            # images=Item.query.filter_by(id=customer_products[product_id]).first().image_url
+        ).id
+        print(f"product id:{stripe_product_id}")
+        price_id = stripe.Price.create(
+            unit_amount=int(Item.query.filter_by(id=product_id).first().price)*100,
+            currency="zar",
+            product=stripe_product_id,
+        ).id
+        print(f"price id:{price_id}")
+        new_item = {
+            "price": price_id,
+            "quantity": 1
+        }
+        customer_items.append(new_item)
+        print(f"customer items:{customer_items}")
+
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=customer_items,
+            mode='payment',
+            success_url='http://127.0.0.1:5000/success.html',
+            cancel_url='http://127.0.0.1:5000/cancel.html',
+        )
+    except Exception as e:
+        return str(e)
+
+    return redirect(checkout_session.url, code=303)
+
+
+@app.route("/checkout")
+def checkout():
+    return render_template("checkout.html")
+
+
+@app.route("/cancel")
+def cancel():
+    return render_template("cancel.html")
+
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
