cb1c6209c4715d9d7d5c0052e7351753019dbb96
Sequence-94 <53806574+Sequence-94@users.noreply.github.com>
2023-09-10 16:31:02 +0200

    updated success and cancel endpoints

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,11 +1,10 @@
-import os
 from markupsafe import Markup
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_bootstrap import Bootstrap5
 from flask_login import LoginManager
-import os
+
 
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
